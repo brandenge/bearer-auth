dcc8dfb0392062cdb41faefe31cd88f9c422cc73
Branden Ge <86636108+brandenge@users.noreply.github.com>
2022-09-29 08:01:45 -0500

    All tests passing

--- src/auth/router/handlers.js
+++ src/auth/router/handlers.js
@@ -1,17 +1,17 @@
 'use strict';
 
-const { Users } = require('../models/index.js');
+const { users } = require('../models/index.js');
 
 async function handleSignup(req, res, next) {
   try {
-    let userRecord = await Users.create(req.body);
+    let userRecord = await users.create(req.body);
     const output = {
       user: userRecord,
       token: userRecord.token
     };
-    res.status(200).json(output);
+    res.status(201).json(output);
   } catch (e) {
-    console.error(e);
+    console.error('Error in handleSignup:', e.message);
     next(e);
   }
 }
@@ -19,34 +19,39 @@ async function handleSignup(req, res, next) {
 async function handleSignin(req, res, next) {
   try {
     const user = {
-      user: request.user,
-      token: request.user.token
+      user: req.user,
+      token: req.user.token
     };
     res.status(200).json(user);
   } catch (e) {
-    console.error(e);
+    console.error('Error in handleSignin:', e.message);
     next(e);
   }
 }
 
 async function handleGetUsers(req, res, next) {
   try {
-    const userRecords = await Users.findAll({});
+    const userRecords = await users.findAll();
     const list = userRecords.map(user => user.username);
     res.status(200).json(list);
   } catch (e) {
-    console.error(e);
+    console.error('Error in handleGetUsers:', e.message);
     next(e);
   }
 }
 
 function handleSecret(req, res, next) {
-  res.status(200).text("Welcome to the secret area!");
+  res.status(200).send("Welcome to the secret area!");
+}
+
+function handleHello(req, res, next) {
+  res.status(200).send(`Hello from ${req.name}! This route is now secured by HTTP Basic authentication`)
 }
 
 module.exports = {
   handleSignup,
   handleSignin,
   handleGetUsers,
-  handleSecret
+  handleSecret,
+  handleHello,
 };
